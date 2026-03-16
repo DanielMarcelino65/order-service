19cf0201dbe0bbb39f90bab0a25d96dd71ef2cc1
DanielMarcelino65 <danmarcelino6573@gmail.com>
2026-03-16 10:18:40 -0300

    :bug: revalidate path every component load

--- src/components/molecules/os-cars-grid.tsx
+++ src/components/molecules/os-cars-grid.tsx
@@ -1,12 +1,17 @@
 'use client';
 import { getAllOS } from '@/app/actions/os';
 import { OsCard } from '../atoms/os-card';
+import { useEffect } from 'react';
+import { revalidatePath } from 'next/cache';
 
 type OsCardsGridProps = {
   osList: Awaited<ReturnType<typeof getAllOS>>;
 };
 
 export default function OsCardsGrid({ osList }: OsCardsGridProps) {
+  useEffect(() => {
+    revalidatePath('/');
+  }, []);
   return (
     <div className="grid mt-5 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {osList.data &&
